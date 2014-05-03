6a5de6e0fb7bcc854929b222f8a2c9e3a1847d9c
TanjaM <malic.tanja@gmail.com>
2014-05-03 18:11:02 +0200

    DPLL

--- 03_DPLL.py
+++ 03_DPLL.py
@@ -19,74 +19,118 @@ except NameError:
 
 #f je logicni izraz v KNO obliki
 #literali so seznam literalov, za katere vemo vrednosti
-def DPLL(f, literali):
-           
+def DPLL(f, literali, lahkoDodajamVLiterale):
     ostalo = [] #seznam ORov, kjer ne vemo vrednosti niti enega literala notri
     for i in f.seznam:
-##        print "obdelujem ", i
-##        print "literali ", literali
-##        print "ostalo ", ostalo
-        
+      
         #dolocimo literale
         if isinstance(i, bool.Var):
-##            print "je literal ", i
-            if not i in literali:
-                literali[i] = True
-            elif not literali[i]:
-                return False
+            if not str(i) in literali and lahkoDodajamVLiterale:
+                literali[str(i)] = True
+            elif str(i) in literali:
+                if not literali[str(i)]:
+                    return False
         
         #dolocimo not literale
         elif isinstance(i, bool.NOT):
-##            print "je not literal ", i.vrednost
-            if not i.vrednost in literali:
-                literali[i.vrednost] = False        
-            elif literali[i.vrednost]:
-                return False
+            if not str(i.vrednost) in literali and lahkoDodajamVLiterale:
+                literali[str(i.vrednost)] = False
+            elif str(i.vrednost) in literali:
+                if literali[str(i.vrednost)]:
+                    return False
 
         #dolocimo kar nam ostane
         else:
-##            print "je ostalo ", i
-            n = False
+            vSeznamuLiteralov = False
             for j in i.seznam:
-                if j in literali:
-                    n=True
-            if not n:
+                if isinstance(j, bool.Var) and str(j) in literali:
+                    if literali[str(j)]:
+                        vSeznamuLiteralov=True
+            if not vSeznamuLiteralov:
                 ostalo.append(i)
-
-##    print "konec for"
-##    print "literali ", literali
-##    print "ostalo ", ostalo
+    
     if len(ostalo)==0:
         return literali
-    else:
-        #spremenljivkam potrebno dolociti vrednosti
-        #gemo kar po vrsti in recemo, da je prvi True
-        #ce se ne izide s True, probamo s False
-        #tako probamo za vse elemente seznama ostalo
-        #ce se eden izmed elemntov seznama ostalone izide sledi ni resitve
-        
-        while len(ostalo)>0:
+    #spremenljivkam potrebno dolociti vrednosti - BF
+    else:        
+        seLahkoSpremeni = []    #seznam, ki hrani tiste spr, ki jih lahko spreminjamo   
+        niResitve = False
+        while len(ostalo)>0 and not niResitve:
             k = ostalo[0]
             #preverimo ali se kateri izmed nastopajocih nahaja v literalih
-            n = False
+            #ali ce je sestavljena iz samih NOT
+            #ustvarimo nov (poenostavljen) OR seznam
+            vSeznamuLiteralov = False
+            or_seznam = []
+            count = 0
             for j in k.seznam:
-                if j in literali:
-                    n=True
-            #ce se ne nahaja v literalih vstopimo v ta OR in ga resimo
-            if not n:
-                if isinstance(k.seznam[0], bool.NOT):
-                    literali[k.seznam[0].vrednost] = True
-                else:
-                    literali[k.seznam[0]] = True
-                true = DPLL(k, literali)
-                if not true:
-                    if isinstance(k.seznam[0], bool.NOT):
-                        literali[k.seznam[0].vrednost] = False
-                    else:
-                        literali[k.seznam[0]] = False
-                    true = DPLL(k, literali)
-            del ostalo[0]
-    return true
+                if isinstance(j, bool.Var):
+                    if str(j) in literali:
+                        vSeznamuLiteralov=True
+                    elif str(j) not in seLahkoSpremeni :
+                        seLahkoSpremeni.append(str(j))
+                    or_seznam.append(j)
+                elif isinstance(j, bool.NOT):
+                    if str(j.vrednost) in literali:
+                        if literali[str(j.vrednost)]:
+                            count += 1
+                    elif str(j.vrednost) not in seLahkoSpremeni:
+                        seLahkoSpremeni.append(str(j.vrednost))
+                        or_seznam.append(j)
+                if vSeznamuLiteralov and not DPLL(bool.OR([j]),literali,False):
+                    niResitve = True
+                elif vSeznamuLiteralov:
+                    or_seznam = []
+            
+            if count==len(k.seznam):    #sami NOT, ki imajo znane literale, sledi
+                niResitve = True
+           
+            else:
+                k=bool.OR(or_seznam)
+                if (not vSeznamuLiteralov) and not niResitve and len(k.seznam)!=0:
+                    key = k.seznam[0]
+                    keyNOT=""
+                    if isinstance(key, bool.NOT):
+                        keyNOT = str(k.seznam[0].vrednost)
+                    true = ""
+                    if isinstance(key, bool.NOT) and str(keyNOT) in seLahkoSpremeni:
+                        literali[keyNOT] = True
+                        true = DPLL(k, literali, False)
+                    elif str(key) in seLahkoSpremeni:
+                        literali[str(key)] = True
+                        true = DPLL(k, literali, False)
+                    if not true:
+                        if isinstance(key, bool.NOT) and str(keyNOT) in seLahkoSpremeni:
+                            literali[keyNOT] = False
+                            true = DPLL(k, literali, False)
+                        elif str(key) in seLahkoSpremeni:
+                            literali[str(key)] = False
+                            true = DPLL(k, literali, False)
+                del ostalo[0]
+        if niResitve:
+            return False
+        else:
+            return literali
+
+def DPLL_sort(f):
+    f_sorted = []
+    count = 0
+    for i in f.seznam:
+        if isinstance(i, bool.Var) or isinstance(i, bool.NOT):
+            f_sorted.insert(0,i)
+            count +=1
+        else:
+            pos = len(f_sorted)
+            if pos>0 and pos>count:
+                while len(i.seznam)<len(f_sorted[pos-1].seznam) and pos>count:
+                    pos -= 1
+            if pos==len(f_sorted):
+                f_sorted.append(i)
+            else:
+                f_sorted.insert(pos,i)
+    f_cno_sorted = bool.AND(f_sorted)
+    print "formula sorted: ", f_cno_sorted
+    return DPLL(f_cno_sorted,{}, True)
 
 
 #testni primer
@@ -97,11 +141,20 @@ s = bool.Var('s')
 t = bool.Var('t')
 u = bool.Var('u')
 
-f_cno = bool.AND([p, bool.OR([q,r]), bool.OR([bool.NOT(r),t]), bool.OR([bool.NOT(u), p])])
+##f_cno = bool.AND([p,q,bool.OR([p,r]),bool.OR([u,s,p]), bool.OR([q,r]), bool.OR([p,q,r,s,t]),bool.OR([bool.NOT(r),t]), bool.OR([bool.NOT(u), p])])
+
+
+f_cno = bool.AND([bool.OR([q,r]), bool.OR([r,bool.NOT(p)]),p])
+##f_cno = bool.AND([p,q,r, bool.NOT(r)])
+##f_cno = bool.AND([bool.OR([p,bool.NOT(q)]),bool.OR([bool.NOT(p),q])])
+
+
+f_cno=bool.AND([p,q, bool.OR([bool.NOT(p),bool.NOT(q)])])
+
 
 print "formula: ", f_cno
 ##print"==============================="
-res = DPLL(f_cno,{})
+res = DPLL_sort(f_cno)
 ##print"==============================="
 print "rezultat: ", res
     
