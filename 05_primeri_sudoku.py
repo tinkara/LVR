53d07516898a21b30661654adee86528b3767177
TanjaM <malic.tanja@gmail.com>
2014-05-06 20:59:52 +0200

    sudoku dodan primer

--- 05_primeri_sudoku.py
+++ 05_primeri_sudoku.py
@@ -7,6 +7,7 @@ try:
 except NameError:
     basestring = str
 bool = __import__ ('01_boolove_formule')
+red = __import__('02_redukcija_na_SAT')
 
 '''lep izpis sudoka'''
 def Sudoku_print(sudoku):
@@ -40,4 +41,24 @@ sudoku1 = [[None, 8, None, 1, 6, None, None, None, 7],
 			[None, None, 6, 7, None, 1, 9, None, 3],
 			[7, None, None, None, 9, 6, None, 4, None]]
 
+sudoku2 = [[6,9,2,3,4,8,1,5,7],
+           [7,4,3,5,6,1,9,8,2],
+           [8,1,5,2,9,7,6,3,4],
+           [5,8,6,1,2,3,7,4,9],
+           [4,3,1,7,8,9,5,2,6],
+           [2,7,9,6,5,4,8,1,3],
+           [9,5,8,4,7,2,3,6,1],
+           [1,2,7,8,3,6,4,9,5],
+           [3,6,4,9,1,5,2,7,8]]
+
 print Sudoku_print(sudoku1)
+
+sudoku_SAT = red.Sudoku(sudoku1)
+print sudoku_SAT
+
+sudoku_SAT_CNO = sudoku_SAT.cno()
+print sudoku_SAT_CNO
+
+
+
+
